9c5a1e8282a55427bffad834502d0f5e6659e6b6
zhen9420 <128135758+zhen9420@users.noreply.github.com>
2023-03-30 23:06:16 +0800

    1

--- 031/FileName.cpp
+++ 031/FileName.cpp
@@ -14,15 +14,15 @@ int main(void) {
 		a[i].bmi = (a[i].height - 100) * 0.9 * 2;
 	}
 	for (int i = 0; i < x; i++) {
-		if (a[i].bmi * 1.1 >a[i].weight&&a[i].bmi*0.9<a[i].weight) {
+		if (a[i].bmi * 0.1 >a[i].weight-a[i].bmi && a[i].bmi - a[i].weight<0.1 * a[i].bmi) {
 			cout << "You are wan mei!";
 			if (i != x - 1) cout << endl;
 		}
-		else if (a[i].bmi * 1.1 <= a[i].weight) {
+		else if (a[i].bmi * 0.1 <= a[i].weight - a[i].bmi) {
 			cout << "You are tai pang le!";
 			if (i != x - 1) cout << endl;
 		}
-		else if (a[i].bmi * 0.9 >= a[i].weight) {
+		else if (a[i].bmi - a[i].weight >= 0.1 * a[i].bmi) {
 			cout << "You are tai shou le!";
 			if (i != x - 1) cout << endl;
 		}
