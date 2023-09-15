c430b7219334c9056e6eefeb9c868833b8d2adbf
zhen9420 <128135758+zhen9420@users.noreply.github.com>
2023-09-15 11:03:03 +0800

    1

--- 049/源.cpp
+++ 049/源.cpp
@@ -1,7 +1,17 @@
 #include<iostream>
+#include<vector>
 using namespace std;
-const int num = 10010;
-
+const int N = 1e5 + 10;
+int n = 0;
+vector<int> a;
 int main(void) {
-
+	cin >> n;
+	for (int i = 0; i <n; i++) {
+		int temp;
+		cin >> temp;
+		a[i] = temp * 10;
+	}
+	for (int i = 0; i < n; i++) {
+		cout << a[i];
+	}
 }
